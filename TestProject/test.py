6042e690c65ca5f748737d5e9a1b23e68ea83025
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-04-16 09:57:48 -0400

    Added the MIT python files for 6.0001 Lec1 to Lec12 was included.

--- TestProject/test.py
+++ TestProject/test.py
@@ -1,7 +1,6 @@
 #Welcome to the test.py script. This is to make sure I can run very basic stuff before I go on to take the class
 #TODO
     #add notebooks folder here and push to master. 
-    #learn how to fork and edit and all that good stuff with the tutorial and using this test.py script
 
 import numpy as np #had to use conda install numpy - and conda install jupyter to open my previous notebooks
 print("This is a test project")
