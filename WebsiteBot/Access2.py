5753cdfc509d292ff02b8c12038350f3fa991b27
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-04-17 09:22:58 -0400

    bs4 installed to use beautiful soup

--- WebsiteBot/Access2.py
+++ WebsiteBot/Access2.py
@@ -3,7 +3,9 @@
 ######################################################
 
 from selenium import webdriver 
+from bs4 import BeautifulSoup 
 
 #To configure webdriver to use Chrome browser, we have to set the path to chromedriver
     #PATH to installed driver: "C:\\Users\\fabri\\miniconda3\\Lib\\site-packages\\selenium\\webdriver\\chrome\\chromedriver.exe"
-driver = webdriver.Chrome("C:\\Users\\fabri\\miniconda3\\Lib\\site-packages\\selenium\\webdriver\\chrome\\chromedriver.exe")
+#driver = webdriver.Chrome("C:\\Users\\fabri\\miniconda3\\Lib\\site-packages\\selenium\\webdriver\\chrome\\chromedriver.exe")
+
