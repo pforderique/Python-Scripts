707911202a5c840adb0833ddd52e0968404e6f12
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-04-18 09:59:14 -0400

    pset0 completed

---
+++ MIT 6.0001/psets/ps0/ps0/ps0.py
@@ -0,0 +1,22 @@
+#Piero Orderique
+#6.0001
+#pset0
+
+#Did not collaborate with anyone
+
+import math
+import numpy as np
+
+print("input a number x: ")
+x = float(input())
+print("\ninput a number y: ")
+y = float(input())
+
+print("x**y = ", x**y)
+print("log2(x) = ", np.log2(x))
+
+##This is me testing some things (ignore)
+# number= 20
+# plusTen = number + 10
+# plusTwenty = number + 20
+# print ("If we add 10 and 20 to your number, we get %s and %s" % (plusTen, plusTwenty))
