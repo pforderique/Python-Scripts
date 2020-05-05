379cc7974f8742607a44e5ad895b6aa98aaa04bf
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-05-05 10:12:09 -0400

    Working Towers of Hanoi problem

---
+++ RandomProjects/Towers of Hanoi/hanoi.py
@@ -0,0 +1,21 @@
+#   Solving the world famous towers of hanoi problem 
+
+counter = 0
+def hanoi(n,start,middle,end):
+    global counter
+    if n == 1:
+        counter += 1  
+        print('Move disk from peg',start,'to peg',end)
+    else:
+        counter += 1
+        hanoi(n-1,start,end,middle)
+        print('Move disk from peg',start,'to peg',end)
+        hanoi(n-1,middle,start,end)
+        
+
+if __name__ == "__main__":
+    print('================= TOWERS OF HANOI ==================\n')
+    print('================= TOWERS OF HANOI:',i,'disks ==================')
+    hanoi(3,1,2,3)
+    print('Number of Steps:',counter)
+    counter = 0
