d4967c793e9c69c8542add5dc3d4e04059a1814a
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-08-02 18:07:28 -0400

    added comp_base dictionary for going from mrna to dna bases

--- RandomScripts/BiologyScripts/mrna-to-protein-decoder.py
+++ RandomScripts/BiologyScripts/mrna-to-protein-decoder.py
@@ -3,6 +3,7 @@
 # Piero Orderique
 # 2 August 2020
 
+#contains the dictionary for converting codons to proteins
 codon_dict = {'AUG':'Met', 'AUA':'Ile', 'AUC':'Ile', 'AUU':'Ile', 
               'AGA':'Arg', 'AGG':'Arg', 'AGC':'Ser', 'AGU':'Ser',
               'ACG':'Thr', 'ACA':'Thr', 'ACC':'Thr', 'ACU':'Thr',
@@ -19,3 +20,11 @@ codon_dict = {'AUG':'Met', 'AUA':'Ile', 'AUC':'Ile', 'AUU':'Ile',
               'CGG':'Arg', 'CGA':'Arg', 'CGC':'Arg', 'CGU':'Arg',
               'CCG':'Pro', 'CCA':'Pro', 'CCC':'Pro', 'CCU':'Pro',
               'CAG':'Gln', 'CAA':'Gln', 'CAC':'His', 'CAU':'His'}
+
+#contains complimentary bases from mrna to dna (reverse transcriptase)
+comp_base = {'A':'T', 'U':'A', 'C':'G', 'G':'C'}
+
+mrna = "AUGCAUAUACAC"
+
+def reverseTranscriptase(mrna):
+    pass
