e098c538f379a9756149bfa59fdd7281aadae907
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-08-31 09:49:37 -0400

    added structure of linked list

---
+++ Coding_Practice/Data-Structures/linked-lists/struct.py
@@ -0,0 +1,69 @@
+# Coded by tutorial by Brian Faure
+class node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class linked_list:
+    def __init__(self):
+        self.head = node()
+
+    def append(self, data):
+        new_node = node(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
+    
+    def length(self):
+        cur = self.head
+        total = 0 
+        while cur.next != None:
+            total += 1
+            cur = cur.next
+        return total
+    
+    def display(self):
+        elems = []
+        cur_node = self.head
+        while cur_node.next!=None:
+            cur_node = cur_node.next
+            elems.append(cur_node.data)
+        print(elems)
+
+    def get(self, index):
+        if index >= self.length():
+            print("ERROR: index out of range")
+            return None
+        cur_idx = 0
+        cur_node = self.head
+        while True :
+            cur_node = cur_node.next
+            if cur_idx == index:
+                return cur_node.data
+            cur_idx += 1
+
+    def erase(self, index):
+        if index >= self.length():
+            print("ERROR: index out of range")
+            return None
+        cur_idx = 0
+        cur_node = self.head
+        while True :
+            last_node = cur_node
+            cur_node = cur_node.next
+            if cur_idx == index:
+                last_node.next = cur_node.next
+                return cur_node.data
+            cur_idx += 1
+        
+if __name__ == "__main__":
+    my_list = linked_list()
+    my_list.display()
+    my_list.append(1)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(4)
+    my_list.erase(1)
+    my_list.display()
+    print("element at 2nd index: %s" % my_list.get(2))
