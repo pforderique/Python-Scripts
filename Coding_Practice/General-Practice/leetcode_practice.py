c967744aef57e079e2a84d864bf87de51f36708e
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-12-13 17:22:49 -0500

    diagonal sum of matrix

--- Coding_Practice/General-Practice/leetcode_practice.py
+++ Coding_Practice/General-Practice/leetcode_practice.py
@@ -26,7 +26,22 @@ class Solution:
             res += gettimebetweenpoints(points[idx], points[idx+1])
         return res #60 ms
 
+    def diagonalSum(self, mat) -> int:
+        side = len(mat)
+        row = col = res = 0
+        col2 = side - 1
+        while row < side:
+            res += (mat[row][col] + mat[row][col2])
+            row += 1
+            col += 1
+            col2 -= 1
+        if side%2 == 0:
+            return res
+        return res - mat[side//2][side//2] #124 ms
+
+
 if __name__ == "__main__":
     sol = Solution()
     # print(sol.findNumbers([12,345,2,6,7896]))
-    print(sol.minTimeToVisitAllPoints([[1,1],[3,4],[-1,0]]))
+    # print(sol.minTimeToVisitAllPoints([[1,1],[3,4],[-1,0]]))
+    print(sol.diagonalSum([[1,2,3],[4,5,6],[7,8,9]]))
