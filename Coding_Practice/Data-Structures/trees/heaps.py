1f2bd6fae925d334af005008a1135e56a97292e9
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-12-15 14:17:47 -0500

    intro heap

---
+++ Coding_Practice/Data-Structures/trees/heaps.py
@@ -0,0 +1,11 @@
+# Heap Practice Module 
+# Dec 15 2020
+
+def array_to_heap(arr):
+    '''
+    returns a heap data structure 
+    '''
+    pass
+
+if __name__ == "__main__":
+    h = [9,5,2,1,4,3,7,8,6]
