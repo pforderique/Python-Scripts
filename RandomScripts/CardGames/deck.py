598c4e288e0bfcb7ee8cc5e5a1fdf4ad96b4d49b
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-12-19 12:31:19 -0500

    added functions that need to be implemented

--- RandomScripts/CardGames/deck.py
+++ RandomScripts/CardGames/deck.py
@@ -5,12 +5,21 @@
 from card import Card
 class Deck():
     def __init__(self) -> None:
-        '''Create 52 card deck'''
+        """Create 52 card deck"""
         self.deck = [Card(rank, suit) for rank in Card.ranks for suit in Card.suits]
 
     def __str__(self) -> str:
         readableDeck = [str(c) for c in self.deck]
         return str(readableDeck)
+
+    def shuffle(self, times = 1):
+        """mixes up card order in the deck"""
+        pass
+
+    def sort(type = 'bysuit', order = 'ascending'):
+        """sorts deck by suit - [all Hs, Ss, Ds, Cs]"""
+        pass
         
 if __name__ == "__main__":
-    print(Deck())
+    deck1 = Deck()
+    print(deck1)
