e2d75affbcc4dc61ed22430cf93a39633db45b7c
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-12-19 12:06:12 -0500

    card exception implemented

--- RandomScripts/CardGames/card.py
+++ RandomScripts/CardGames/card.py
@@ -4,19 +4,26 @@
 
 class Card:
     # suits = {hearts, spades, diamonds, clubs}
+    suits = ('H', 'S', 'D', 'C')
     # ranks = {2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A}
-
-    # (rank: value) dictionary lookup
     values = {2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 'J':11, 'Q':12, 'K':13, 'A': 14}
 
     def __init__(self, rank: str, suit: str,) -> None:
-        self.suit = suit
         self.rank = rank
         self.value = self.values[rank]
+        suit = suit.capitalize()
+        if suit in self.suits:   
+            self.suit = suit
+        else:
+            raise CardException("Suit not suitable")
 
     def __str__(self) -> str:
-        return self.rank+ " of " + self.suit.capitalize()
+        return self.rank + self.suit
+
+
+class CardException(Exception):
+    pass
 
 # testing
 if __name__ == "__main__":
-    print(Card('J', 'diamonds'))
+    print(Card('J', 'H'))
---
+++ RandomScripts/CardGames/deck.py
@@ -0,0 +1,9 @@
+# deck class for instansiating a standard 52 card deck
+# Written by: Piero Orderique
+# 12/19/2020
+
+import Card
+class Deck():
+    def __init__(self) -> None:
+        '''Create 52 card deck'''
+        
