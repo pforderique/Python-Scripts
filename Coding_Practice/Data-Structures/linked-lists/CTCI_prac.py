f1dd5b341eee80253d75fc4bcee90007689b7acf
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-12-27 15:38:55 -0500

    kth to last

--- Coding_Practice/Data-Structures/linked-lists/CTCI_prac.py
+++ Coding_Practice/Data-Structures/linked-lists/CTCI_prac.py
@@ -17,7 +17,6 @@ class Node:
             n = n.next
         return rep + " -> NONE"
         
-
 # outside user-defined functions
 def delete_node(head: Node, data: int) -> Node:
     if head == None: return None
@@ -40,11 +39,38 @@ def remove_duplicates(head: Node) -> None:
             n.next = n.next.next
         else:
             seen.add(n.next.data)
+            n = n.next
+
+def get_len(head: Node) -> int:
+    '''returns the length of a linked list'''
+    length = 1 
+    n = head
+    while n.next != None:
+        length += 1 
         n = n.next
+    return length
+
+def kth_to_last(head: Node, k: int) -> Node:
+    ''' returns the kth to last node in single linked list'''
+    length = get_len(head)
+    counter = 1
+    curr = head
+    while counter < length - k:
+        curr = curr.next
+        counter += 1
+    return curr
+
+def recursive_kthtolast(head: Node, k: int) -> Node:
+    ''' returns the kth to last node in single linked list - but spicy'''
+
 
 # Driver Code
 if __name__ == "__main__":
     ll = Node(1)
     ll.append_to_tail(2)
     ll.append_to_tail(3)
+    ll.append_to_tail(5)
+    ll.append_to_tail(8)
     print(ll)
+    print("Length:", get_len(ll))
+    print(kth_to_last(ll, 1).data)
