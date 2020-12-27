eef21bea4e875a3616585318652384e2e85574b5
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-12-27 09:12:04 -0500

    messed around with mutability and added __str__ rep

--- Coding_Practice/Data-Structures/linked-lists/CTCI_struct.py
+++ Coding_Practice/Data-Structures/linked-lists/CTCI_struct.py
@@ -9,10 +9,28 @@ class Node:
         self.next = None
         self.data = data
 
-    def appendToTail(self, data):
+    def append_to_tail(self, data):
         end = Node(data)
         n = self
         while(n.next != None):
             n = n.next
         n.next = end
+    
+    def __str__(self):
+        if self.next == None: rep = "Node data: {}\nNext Node: NONE\n".format(self.data)
+        else: rep = "Node data: {}\nNext Node: {}\n".format(self.data, self.next)
+        return rep
 
+# outside user-defined functions
+def delete_node(self, head: Node, data) -> Node:
+    if head == None: return None
+    n = head
+
+
+if __name__ == "__main__":
+    ll = Node("Piero")
+    ll.append_to_tail("Fabrizzio")
+    n = ll
+    n = n.next
+    print(ll)
+    print(n)
