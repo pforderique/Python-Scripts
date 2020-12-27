5d0a125384218d0aaa46d725af319e995c7552c5
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-12-27 08:52:59 -0500

    understood and added the linked list append to tail method

--- Coding_Practice/Data-Structures/linked-lists/CTCI_struct.py
+++ Coding_Practice/Data-Structures/linked-lists/CTCI_struct.py
@@ -1,12 +1,18 @@
 # Structure as presented in CTCI
 # 12 Oct 2020
+# Revisited 27 Dec 2020
 
-# Linked-List Structure
-class node:
+# Linked-List Structure 
+# - access to linked list via reference to the head node
+class Node:
     def __init__(self, data=None):
         self.next = None
         self.data = data
 
     def appendToTail(self, data):
-        self.end = node(data)
-        
+        end = Node(data)
+        n = self
+        while(n.next != None):
+            n = n.next
+        n.next = end
+
