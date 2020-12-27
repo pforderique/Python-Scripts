c0eb20042003bc3983327ea355525ec5f06d6271
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-12-27 08:14:37 -0500

    fib2

--- Coding_Practice/General-Practice/recursion_practice.py
+++ Coding_Practice/General-Practice/recursion_practice.py
@@ -17,12 +17,25 @@ def isPalindrome(num: int):
 # FIBONACCI DP Practice
 def fib1(n):
     '''Worst fib function. Returns nth fib number. Exponential time.'''
-    if n <= 0: return -1
+    if n <= 0: return 0
     if n == 1 or n == 2: return 1
     else: return fib1(n-1)+fib1(n-2)
 
-
+def fib2(n, d={1:1, 2:1}):
+    '''BETTER fib function. Returns nth fib number.'''
+    if n <= 0: return 0
+    if n not in d: 
+        d[n] = fib2(n-1, d)+ fib2(n-2, d)
+        return d[n]
+    else:
+        return d[n]
 
 if __name__ == "__main__":
     # print(isPalindrome(2002), isPalindrome(1492), isPalindrome(29492), isPalindrome(30201))
-    print(fib1(20))
+    import datetime
+    begin_time = datetime.datetime.now()
+    print(fib1(25))
+    print('time for fib1:',datetime.datetime.now() - begin_time)
+    begin_time = datetime.datetime.now()
+    print(fib2(25))
+    print('time for fib2:',datetime.datetime.now() - begin_time)
