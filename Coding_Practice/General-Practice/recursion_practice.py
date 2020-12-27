e9102a448b353a19a0830dda8b5c658a1016ef4f
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-12-27 08:04:22 -0500

    fib1

--- Coding_Practice/General-Practice/recursion_practice.py
+++ Coding_Practice/General-Practice/recursion_practice.py
@@ -14,5 +14,15 @@ def isPalindrome(num: int):
     if str_num[0] == str_num[-1]: return isPalindrome(str_num[1:len(str_num)-1])
     return False
 
+# FIBONACCI DP Practice
+def fib1(n):
+    '''Worst fib function. Returns nth fib number. Exponential time.'''
+    if n <= 0: return -1
+    if n == 1 or n == 2: return 1
+    else: return fib1(n-1)+fib1(n-2)
+
+
+
 if __name__ == "__main__":
     # print(isPalindrome(2002), isPalindrome(1492), isPalindrome(29492), isPalindrome(30201))
+    print(fib1(20))
