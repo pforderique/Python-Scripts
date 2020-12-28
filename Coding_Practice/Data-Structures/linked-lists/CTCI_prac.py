a3ce7ef8b0eee22c0320322ffc03bcec1c337526
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-12-27 21:34:27 -0500

    partially working

--- Coding_Practice/Data-Structures/linked-lists/CTCI_prac.py
+++ Coding_Practice/Data-Structures/linked-lists/CTCI_prac.py
@@ -73,13 +73,33 @@ def kth_to_last2(head: Node, k: int) -> Node: # what if we don't know the length
         p1 = p1.next
     return p1
 
+def sum_lists(head1: Node, head2: Node) -> Node:
+    '''adds two nums represented by linked lists and outputs linked list result'''
+    n1 = head1
+    n2 = head2
+    curr_sum = n1.data + n2.data
+    res = Node(curr_sum % 10)
+    carry_over = curr_sum // 10
+    while (n1.next and n2.next) or carry_over != 0: # while there are more nodes OR carry over bits
+        current_sum = n1.next.data + n2.next.data + carry_over
+        remainder = current_sum % 10 
+        carry_over = current_sum // 10
+        res.append_to_tail(remainder)
+        # iterate to next
+        n1 = n1.next
+        n2 = n2.next
+    # works, but not if we need to go one further digit OR what if we need to extend further? fix this later!
+    
+    return res
+        
 # Driver Code
 if __name__ == "__main__":
     ll = Node(1)
-    ll.append_to_tail(2)
+    ll.append_to_tail(4)
     ll.append_to_tail(3)
-    ll.append_to_tail(5)
-    ll.append_to_tail(8)
     print(ll)
-    print("Length:", get_len(ll))
-    print(kth_to_last2(ll, 1).data)
+    LL = Node(2)
+    LL.append_to_tail(7)
+    LL.append_to_tail(6)
+    print(LL)
+    # print(sum_lists(ll, LL))
