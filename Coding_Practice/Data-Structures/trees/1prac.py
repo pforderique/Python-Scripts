d7109b666fa6e3fa581541c4d9c28bb02e44ac87
pforderique <porderiq@mit.edu>
2021-01-12 14:06:27 -0500

    working treeify function. Correct BST property!!

--- Coding_Practice/Data-Structures/trees/1prac.py
+++ Coding_Practice/Data-Structures/trees/1prac.py
@@ -12,11 +12,28 @@ class Node:
     def __str__(self) -> str:
         return str(self.data)
 
-class TreeNode(Node):
+class TreeNode():
     def __init__(self, data) -> None:
+        self.data = data
         self.left = None
         self.right = None
-        super().__init__(data, adjacent=[self.left, self.right])
+        self.visited = False
+
+    def print_tree(self):
+        '''uses DFS to print'''
+        if self == None: return
+        self.visited = True
+        print(self)
+        # now do it for the children
+        if self.left and not self.left.visited:
+            self.left.visited = True
+            self.left.print_tree()
+        if self.right and not self.right.visited:
+            self.right.visited = True
+            self.right.print_tree()
+
+    def __str__(self) -> str:
+        return str(self.data)
 
 # 4.1 Route Between Nodes : Design Algo that returns True iff there exists a path from S->E
 # I wil be using BFS for path finding
@@ -40,23 +57,47 @@ def existsPath(S:Node, E:Node) -> bool:
     return False
 
 # 4.2 Minimal Tree : given sorted increasing arr, create a Binary Search Tree with min height
-def treeify(arr:list) -> Node:
+def treeify(arr:list) -> TreeNode:
     # Bases Cases
     if len(arr) == 0: return None
-    if len(arr) == 1: return Node(arr[0])
+    if len(arr) == 1: return TreeNode(arr[0])
     # else get root create its subtrees
     mid = len(arr)//2 
-    root = Node(arr[mid])
-    
+    root = TreeNode(arr[mid])
+    root.left = treeify(arr[:mid])
+    root.right = treeify(arr[mid+1:])
+    # now return the root node
+    return root
 
 # ____________________________________________________________________
-# Testing
+# Test Data:
 
 # Graph
     # S -> x -> y -> E
-    # |-> redherring
-E = Node("E")
-S = Node("S", [Node("x", [Node("y", [E])]), Node("redherring")])
+    # |-> andre
+# E = Node("E")
+# S = Node("S", [Node("x", [Node("y", [E])]), Node("andre")])
+
+# BST:
+    #      4
+    #     / \
+    #    2   9
+    #   /   / \
+    #  1   4   11
+n1 = TreeNode(1)
+n2 = TreeNode(4)
+n3 = TreeNode(11)
+n4 = TreeNode(2)
+n4.left = n1
+n5 = TreeNode(9)
+n5.left = n2
+n5.right = n3
+root = TreeNode(4)
+root.left = n4
+root.right = n5
+arr = [1,2,4,4,9,11]
 
 if __name__ == "__main__":
-    pass
+    root = treeify(arr=arr)
+    root.print_tree()
+    
