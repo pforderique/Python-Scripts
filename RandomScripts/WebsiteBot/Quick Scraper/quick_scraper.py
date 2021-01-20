7e4befe495a44c420ff78746c1a3600779e5f7a0
pforderique <porderiq@mit.edu>
2021-01-20 16:35:31 -0500

    quick scraper successfully navigates to website needed

---
+++ RandomScripts/WebsiteBot/Quick Scraper/quick_scraper.py
@@ -0,0 +1,23 @@
+'''
+Quick Web Scraper
+
+Piero Orderique
+20 Jan 2021
+
+Quick Web Scraper returns a list of everything on a website
+that is associated with an id or class. Very simple and 
+straight forward program for the "quick webscraper" who 
+just wants a list of all Xs included on one site.
+'''
+from bs4 import BeautifulSoup 
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager # forget about updating my guy
+
+# personal interests : in my case, I want a list of all OOD methods listed on a site
+WEBSITE = "https://refactoring.guru/design-patterns/cpp"
+design_methods = []
+
+# create web driver and navigate to website you need
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(WEBSITE)
+
