a303d30d583a0d122944609ff8bd36f5f23066c4
pforderique <porderiq@mit.edu>
2021-02-11 08:34:35 -0500

    server code complete

--- Coding_Practice/Libraries/Socket/server.py
+++ Coding_Practice/Libraries/Socket/server.py
@@ -1,12 +1,13 @@
 import socket
 import threading
 
-# pick port that is not being used anywhere else
-PORT = 5050 
 
-# 192.168.1.19
-SERVER = socket.gethostbyname(socket.gethostname())
+HEADER = 64     # length of message that we are receiving (in bytes)
+PORT = 5050     # pick port that is not being used anywhere else
+SERVER = socket.gethostbyname(socket.gethostname()) # 192.168.1.19
 ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 # create a socket for AF_INET (IPv4) addresses
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,14 +23,27 @@ def handle_client(conn, addr):
         # we CANNOT move past this line until we receiver information
         # this is WHY it is important to run these in different threads -
         #   so we don't block other clients!
-        msg = conn.recv()
+        msg_length = conn.recv(HEADER).decode(FORMAT) # decode msg from byte format to string
+
+        # use msg length to now accurately get how many bytes we are going to receive 
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+
+        print(f"[{addr}] {msg}") 
+
+    conn.close()
 
 def start():
     # start listening for new connection
     server.listen() 
+    print(f"[LISTENING] Server is listening on {SERVER}")
+
     while True:
         # get port and IP addr of the connection
         conn, addr = server.accept()
+        print(type(conn), type(addr))
         # pass that connection to handle client
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         # and start that thread
