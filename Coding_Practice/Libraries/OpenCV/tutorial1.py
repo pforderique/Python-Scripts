377a2c5064c437929a387b10b1ffa86b212c47e7
pforderique <porderiq@mit.edu>
2021-02-15 09:26:40 -0500

    init commit for opencv tutorial

---
+++ Coding_Practice/Libraries/OpenCV/tutorial1.py
@@ -0,0 +1,34 @@
+'''
+Tutorial 1: Loading, editing, writing
+'''
+
+import cv2
+
+'''read in image 
+    -1: normal, ignore trans #default
+     0: grayscale
+     1: loads as such including alpha channel
+'''
+img = cv2.imread('assets/MIT_Dome.jpg', 0)
+
+''' Resizing, rotating, etc '''
+#* Resize by pixel
+# img = cv2.resize(img, (400, 400))
+
+#* OR to resize by scaling (by half in this case)
+img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+
+#* ROTATE 
+# img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+
+#* WRITE IMAGE (save)
+cv2.imwrite('assets/new_img.png', img)
+
+'''
+load image in a window. Set window name as first arg
+wait at most (0=infinity) time for user to click any key
+destroy every window
+'''
+cv2.imshow('Image Window Name Here', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
